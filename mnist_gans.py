ebb64415771aeb8174c22a19b784f569a99890ea
Harshit Awasthi <harshitwas@gmail.com>
2018-02-13 20:13:29 +0530

    Update mnist_gans.py

--- mnist_gans.py
+++ mnist_gans.py
@@ -106,3 +106,47 @@ d_vars = [variable for variable in t_vars if variable.name.startswith('discrimin
 
 d_train_opt = tf.train.AdamOptimizer(learning_rate).minimize(d_loss, var_list = d_vars)
 g_train_opt = tf.train.AdamOptimizer(learning_rate).minimize(g_loss, var_list = g_vars)
+
+batch_size = 100
+epochs = 100
+samples = []
+losses = []
+saver = tf.train.Saver(var_list = g_vars)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for e in range(epochs):
+        for ii in range(mnist.train.num_examples//batch_size):
+            batch = mnist.train.next_batch(batch_size)
+            
+            # Get images, reshape and rescale to pass to D
+            batch_images = batch[0].reshape((batch_size, 784))
+            batch_images = batch_images*2 - 1
+            
+            # Sample random noise for G
+            batch_z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+            
+            # Run optimizers
+            _ = sess.run(d_train_opt, feed_dict={input_real: batch_images, input_z: batch_z})
+            _ = sess.run(g_train_opt, feed_dict={input_z: batch_z})
+        
+        # At the end of each epoch, get the losses and print them out
+        train_loss_d = sess.run(d_loss, {input_z: batch_z, input_real: batch_images})
+        train_loss_g = g_loss.eval({input_z: batch_z})
+            
+        print("Epoch {}/{}...".format(e+1, epochs),
+              "Discriminator Loss: {:.4f}...".format(train_loss_d),
+              "Generator Loss: {:.4f}".format(train_loss_g))    
+        # Save losses to view after training
+        losses.append((train_loss_d, train_loss_g))
+        
+        # Sample from generator as we're training for viewing afterwards
+        sample_z = np.random.uniform(-1, 1, size=(16, z_size))
+        gen_samples = sess.run(
+                       generator(input_z, input_size, n_units=g_hidden_size, reuse=True, alpha=alpha),
+                       feed_dict={input_z: sample_z})
+        samples.append(gen_samples)
+        saver.save(sess, './checkpoints/generator.ckpt')
+
+# Save training generator samples
+with open('train_samples.pkl', 'wb') as f:
+    pkl.dump(samples, f)
